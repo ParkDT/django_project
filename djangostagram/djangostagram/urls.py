7c1c13ee4b60ccd2c31066557aa482d29d5f8cdd
박범수 <bagbeomsu@BeomSooPark.local>
2022-01-02 18:58:31 +0900

    add models

--- djangostagram/djangostagram/urls.py
+++ djangostagram/djangostagram/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from user.views import index ,redirct
+from user.views import index ,redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",index,name="index"),
-    path("redirct", redirct)
+    path("redirect", redirect),
 ]
--- djangostagram/user/models.py
+++ djangostagram/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.db.models.base import Model
 # Create your models here.
+class Dsuser(models.Model):
+    user_id= models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    email=models.EmailField(max_length=254)
+    created_at=models.DateTimeField(auto_now_add=True)
