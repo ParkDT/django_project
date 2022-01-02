b413b87f324105d5df07766bb585f1d77bb61de8
shdq2 <shdq2@naver.com>
2022-01-02 18:26:45 +0900

    Update views.py
    redirect 오타수정

--- djangostagram/user/views.py
+++ djangostagram/user/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render,redirct
+from django.shortcuts import render,redirect
 
 
 def index (request):
     return render(request , "base.html", {"base":"beomsoo"}) 
 
 def redirct(requst):
-    return redirct("index")
+    return redirect("index")
